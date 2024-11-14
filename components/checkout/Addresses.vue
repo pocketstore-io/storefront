2026f958cb0a8039dc609f183d632fa1ab96dbe7
Jonathan Martz <git@jonathan-martz.de>
2024-11-14 15:49:21 +0100

    add more translations fields

--- components/checkout/Addresses.vue
+++ components/checkout/Addresses.vue
@@ -1,33 +1,33 @@
 <template>
   <section v-if="checkoutStep == 'addresses'" class="grid grid-cols-6 gap-3 mx-auto max-w-6xl px-3 py-3">
     <div class="col-span-6 md:col-span-3">
-      <h2 class="font-bold text-lg">Versand</h2>
+      <h2 class="font-bold text-lg">{{ $t('checkout.shipping-address') }}</h2>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Vorname</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.firstname') }}</label>
         <input v-model="shipping.name" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Nachname</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.surname') }}</label>
         <input v-model="shipping.surname" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Straße</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.street') }}</label>
         <input v-model="shipping.street" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Nummer</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.number') }}</label>
         <input v-model="shipping.number" type="number" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">ZIP</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.zip') }}</label>
         <input v-model="shipping.zip" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Stadt</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.city') }}</label>
         <input v-model="shipping.city" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Land</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.country') }}</label>
         <select v-model="shipping.country" class="select select-bordered">
           <option v-for="(country, index) in countries" :value="index">{{ country }}</option>
         </select>
@@ -40,48 +40,47 @@
         <span>{{ $t('checkout.same') }}</span>
       </section>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Vorname</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.firstname') }}</label>
         <input v-model="payment.name" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Nachname</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.surname') }}</label>
         <input v-model="payment.surname" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Straße</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.street') }}</label>
         <input v-model="payment.street" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Nummer</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.number') }}</label>
         <input v-model="payment.number" type="number" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">PLZ</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.zip') }}</label>
         <input v-model="payment.zip" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Stadt</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.city') }}</label>
         <input v-model="payment.city" type="text" class="input input-bordered">
       </div>
       <div class="form-control">
-        <label for="" class="label font-bold text-sm">Land</label>
+        <label for="" class="label font-bold text-sm">{{ $t('checkout.label.country') }}</label>
         <select v-model="payment.country" class="select select-bordered">
           <option v-for="(country, index) in countries" :value="index">{{ country }}</option>
         </select>
       </div>
     </div>
     <div v-else class="col-span-6 md:col-span-3">
-      <h2 class="font-bold text-lg mb-3">Payment</h2>
+      <h2 class="font-bold text-lg mb-3">{{ $t('checkout.payment-address') }}</h2>
       <section class="alert alert-secondary">
         <input v-model="same" type="checkbox" class="checkbox">
         <span>{{ $t('checkout.same') }}</span>
       </section>
     </div>
     <div class="col-span-6 flex justify-between py-3">
-      <button class="btn btn-primary" @click="checkoutStep = 'customer'">Back to Customer</button>
-      <button
-class="btn btn-primary" :disabled="(!valid.payment && !same) || !valid.shipping"
-        @click="checkoutStep = 'payment'"><span>Continue</span>
+      <button class="btn btn-primary" @click="checkoutStep = 'customer'">{{ $t('back.to.customer') }}</button>
+      <button class="btn btn-primary" :disabled="(!valid.payment && !same) || !valid.shipping"
+        @click="checkoutStep = 'payment'"><span>{{ $t('contiue.to.payment') }}</span>
         <Fa :icon="faChevronCircleRight" />
       </button>
     </div>
