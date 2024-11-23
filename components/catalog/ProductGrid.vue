afe87854a5eac3bd0d2795256853d0652dae83e7
Jonathan Martz <git@jonathan-martz.de>
2024-11-23 15:29:39 +0100

    split index page into components

---
+++ components/catalog/ProductGrid.vue
@@ -0,0 +1,28 @@
+<template>
+  <div v-for="product in products" :key="product.id" class="col-span-6 md:col-span-2 py-3">
+    <CatalogProductCard :identifier="product.id" />
+  </div>
+</template>
+
+<script setup>
+import PocketBase from 'pocketbase';
+import { usePocketbaseStore } from '~/stores/pocketbase';
+
+const { categoryName } = defineProps({
+  categoryName: { type: String, required: true }
+});
+
+const store = usePocketbaseStore();
+const { url } = storeToRefs(store);
+const pb = new PocketBase(url.value);
+const category = ref({});
+const products = ref([]);
+
+onMounted(async () => {
+  category.value = await pb.collection('categories').getFirstListItem('name="' + categoryName + '"', {
+    expand: 'products',
+    sort: 'products.created'
+  });
+  products.value = category.value.expand.products;
+});
+</script>
--- pages/impressum.vue
+++ pages/impressum.vue
@@ -8,56 +8,70 @@
     <p>E-Mail: <a href="mailto:impresum@jonathan-martz.de">impressum@jonathan-martz.de </a><br><br></p>
     <h2><strong>Haftungsausschluss:</strong></h2>
     <p><br><br><strong>Haftung für Inhalte</strong><br><br> Die Inhalte unserer Seiten wurden mit größter Sorgfalt
-      erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen.
-      Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
-    verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
-    gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
-    Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen
-    Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer
-    konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte
-    umgehend entfernen. <br><br><strong>Haftung für Links</strong><br><br> Unser Angebot enthält Links zu externen
-    Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch
-    keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
-    Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
-    überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche
-    Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
-    Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
-    <br><br><strong>Urheberrecht</strong><br><br> Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen
-    Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
-    Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw.
-    Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
-    Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet.
-    Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung
-    aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir
-    derartige Inhalte umgehend entfernen. <br><br><strong>Datenschutz</strong><br><br> Die Nutzung unserer Webseite ist
-    in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten
-    (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf
-    freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. <br> Wir
-    weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken
-    aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. <br> Der Nutzung
-    von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht
-    ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die
-    Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von
-    Werbeinformationen, etwa durch Spam-Mails, vor.<br></p>
-  <p><strong>SSL- bzw. TLS-Verschlüsselung</strong></p>
-  <p>Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der Übertragung vertraulicher Inhalte, wie zum Beispiel
-    Bestellungen oder Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL-bzw. TLS-Verschlüsselung. Eine
-    verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des Browsers von “http://” auf “https://”
-    wechselt und an dem Schloss-Symbol in Ihrer Browserzeile. Wenn die SSL- bzw. TLS-Verschlüsselung aktiviert ist,
-    können die Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen werden.</p>
-  <p><strong>Widerspruch gegen Werbe-Mails</strong></p>
-  <p>Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten zur Übersendung von nicht
-    ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit widersprochen. Die Betreiber der Seiten
-    behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa
-    durch Spam-E-Mails, vor.</p>
-  <p><strong>Server-Log-Dateien</strong></p>
-  <p>Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log-Dateien, die Ihr
-    Browser automatisch an uns übermittelt. Dies sind:<br><br> · Browsertyp und Browserversion<br> · verwendetes
-    Betriebssystem<br> · Referrer URL<br> · Hostname des zugreifenden Rechners<br> · Uhrzeit der Serveranfrage<br> ·
-    IP-Adresse<br><br><br> Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen.<br>
-    Grundlage für die Datenverarbeitung ist Art. 6 Abs. 1 lit. f DSGVO, der die Verarbeitung von Daten zur Erfüllung
-    eines Vertrags oder vorvertraglicher Maßnahmen gestattet.</p>
-</section></template>
+      erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr
+      übernehmen.
+      Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen
+      Gesetzen
+      verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder
+      gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige
+      Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den
+      allgemeinen
+      Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis
+      einer
+      konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese
+      Inhalte
+      umgehend entfernen. <br><br><strong>Haftung für Links</strong><br><br> Unser Angebot enthält Links zu externen
+      Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch
+      keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der
+      Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße
+      überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche
+      Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei
+      Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+      <br><br><strong>Urheberrecht</strong><br><br> Die durch die Seitenbetreiber erstellten Inhalte und Werke auf
+      diesen
+      Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+      Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors
+      bzw.
+      Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+      Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter
+      beachtet.
+      Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine
+      Urheberrechtsverletzung
+      aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir
+      derartige Inhalte umgehend entfernen. <br><br><strong>Datenschutz</strong><br><br> Die Nutzung unserer Webseite
+      ist
+      in der Regel ohne Angabe personenbezogener Daten möglich. Soweit auf unseren Seiten personenbezogene Daten
+      (beispielsweise Name, Anschrift oder eMail-Adressen) erhoben werden, erfolgt dies, soweit möglich, stets auf
+      freiwilliger Basis. Diese Daten werden ohne Ihre ausdrückliche Zustimmung nicht an Dritte weitergegeben. <br> Wir
+      weisen darauf hin, dass die Datenübertragung im Internet (z.B. bei der Kommunikation per E-Mail) Sicherheitslücken
+      aufweisen kann. Ein lückenloser Schutz der Daten vor dem Zugriff durch Dritte ist nicht möglich. <br> Der Nutzung
+      von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten durch Dritte zur Übersendung von nicht
+      ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit ausdrücklich widersprochen. Die
+      Betreiber der Seiten behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von
+      Werbeinformationen, etwa durch Spam-Mails, vor.<br>
+    </p>
+    <p><strong>SSL- bzw. TLS-Verschlüsselung</strong></p>
+    <p>Diese Seite nutzt aus Sicherheitsgründen und zum Schutz der Übertragung vertraulicher Inhalte, wie zum Beispiel
+      Bestellungen oder Anfragen, die Sie an uns als Seitenbetreiber senden, eine SSL-bzw. TLS-Verschlüsselung. Eine
+      verschlüsselte Verbindung erkennen Sie daran, dass die Adresszeile des Browsers von “http://” auf “https://”
+      wechselt und an dem Schloss-Symbol in Ihrer Browserzeile. Wenn die SSL- bzw. TLS-Verschlüsselung aktiviert ist,
+      können die Daten, die Sie an uns übermitteln, nicht von Dritten mitgelesen werden.</p>
+    <p><strong>Widerspruch gegen Werbe-Mails</strong></p>
+    <p>Der Nutzung von im Rahmen der Impressumspflicht veröffentlichten Kontaktdaten zur Übersendung von nicht
+      ausdrücklich angeforderter Werbung und Informationsmaterialien wird hiermit widersprochen. Die Betreiber der
+      Seiten
+      behalten sich ausdrücklich rechtliche Schritte im Falle der unverlangten Zusendung von Werbeinformationen, etwa
+      durch Spam-E-Mails, vor.</p>
+    <p><strong>Server-Log-Dateien</strong></p>
+    <p>Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log-Dateien, die
+      Ihr
+      Browser automatisch an uns übermittelt. Dies sind:<br><br> · Browsertyp und Browserversion<br> · verwendetes
+      Betriebssystem<br> · Referrer URL<br> · Hostname des zugreifenden Rechners<br> · Uhrzeit der Serveranfrage<br> ·
+      IP-Adresse<br><br><br> Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen.<br>
+      Grundlage für die Datenverarbeitung ist Art. 6 Abs. 1 lit. f DSGVO, der die Verarbeitung von Daten zur Erfüllung
+      eines Vertrags oder vorvertraglicher Maßnahmen gestattet.</p>
+  </section>
+</template>
 
 <script lang="ts" setup>
 
