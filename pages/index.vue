d3be6bc18eac0c158c0ca06f7f30a15ceb66f1a1
Jonathan Martz <git@jonathan-martz.de>
2024-11-23 16:02:35 +0100

    add cookie first

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,16 @@ import { useBreadcrumbStore } from '~/stores/breadcrumb';
 
 const storeBreadcrumb = useBreadcrumbStore();
 storeBreadcrumb.clear();
+
+useHead({
+    script: [
+        {
+            async: true,
+            defer: true,
+            src: 'https://consent.cookiefirst.com/sites/demo.pocketstore.io-78a0f1a2-ac20-421b-81cf-5dc77f713b67/consent.js'
+        },
+    ],
+});
 </script>
 
 <template>
