dd1960496d77d4c2f9e7853ccc9502dce8a90f47
Jonathan Martz <git@jonathan-martz.de>
2024-11-30 18:47:30 +0100

    update translation plugin

--- plugins/translation.ts
+++ plugins/translation.ts
@@ -1,8 +1,10 @@
 /* eslint-disable */
-// import PocketBase from 'pocketbase'
+import config from '../pocketstore.json';
+import PocketBase from 'pocketbase'
+
 export default defineNuxtPlugin((nuxtApp) => {
     // nuxtApp.$i18n.setMissingHandler(async (locale, message) => {
-    //     const pb = new PocketBase('https://admin.pocketstore.io');
+    //     const pb = new PocketBase(config.domain);
     //     await pb.collection('translations').create({
     //         lang: locale,
     //         key: message,
