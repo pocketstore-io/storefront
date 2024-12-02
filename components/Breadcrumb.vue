e29541f487ce30ab8461302f26f0f6eb5ffb15c4
Jonathan Martz <git@jonathan-martz.de>
2024-12-02 21:52:49 +0100

    update breadcrumb home

--- components/Breadcrumb.vue
+++ components/Breadcrumb.vue
@@ -2,6 +2,7 @@
   <div v-if="loaded" class="breadcrumbs text-sm px-3 py-3 bg-white text-black mb-3 rounded-lg">
     <ul>
       <li>
+        <Fa :icon="faHouse" class="mr-3"/>
         <a href="/de" class="">{{ $t('breadcrumb.home') }}</a>
       </li>
       <li v-for="breadcrumb in breadcrumbs" :key="breadcrumb.id">
@@ -12,6 +13,7 @@
 </template>
 
 <script lang="ts" setup>
+import { faHouse } from '@fortawesome/free-solid-svg-icons';
 import { useLocalStorage } from '@vueuse/core';
 
 const loaded = ref(false);
