b6d52a45de86ad7e656a3ac78242e6c0f7a8d086
Jonathan Martz <git@jonathan-martz.de>
2024-12-08 21:02:19 +0100

    add pwa support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -35,7 +35,8 @@ export default defineNuxtConfig({
   app: {
     head: {
       link: [
-        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'manifest', href: '/manifest.json' }
       ]
     }
   },
---
+++ workbox-config.cjs
@@ -0,0 +1,18 @@
+module.exports = {
+	globDirectory: '.output',
+	globIgnores: [],
+	swDest: 'public/sw.js',
+	runtimeCaching: [
+		{
+			urlPattern: ({ request }) => request.destination === 'image',
+			handler: 'CacheFirst',
+			options: {
+				cacheName: 'images',
+				expiration: {
+					maxEntries: 50,
+					maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
+				},
+			},
+		},
+	],
+};
