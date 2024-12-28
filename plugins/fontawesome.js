04486a24761b70fb900a13d9b338b0e0e519153b
Jonathan Martz <git@jonathan-martz.de>
2024-12-28 19:54:15 +0100

    add fab

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -4,6 +4,7 @@
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false
@@ -11,6 +12,7 @@ config.autoAddCss = false
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
 library.add(fas)
+library.add(fab)
 
 // eslint-disable-next-line
 export default defineNuxtPlugin((nuxtApp) => {
