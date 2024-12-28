e18f37213cb20b7c41a6b71d7a79502c9487a1dd
Jonathan Martz <git@jonathan-martz.de>
2024-12-28 20:10:55 +0100

    fix product card

--- components/catalog/ProductCard.vue
+++ components/catalog/ProductCard.vue
@@ -36,6 +36,6 @@ const pb = new PocketBase(url.value);
 const product = ref({});
 
 onMounted(async () => {
-  product.value = (await pb.collection('products').getFirstListItem('slug="' + identifier + '"'));
+  product.value = (await pb.collection('products').getFirstListItem('id="' + identifier + '"'));
 });
 </script>
