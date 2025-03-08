5deef8ced02c72ebac1993b1e1ab84268fa10e7a
Jonathan Martz <git@jonathan-martz.de>
2025-03-08 22:39:45 +0100

    fix layout css

--- layouts/default.vue
+++ layouts/default.vue
@@ -19,7 +19,7 @@
     </section>
 </template>
 <script setup lang="ts">
-import './main.css';
+import '../main.css';
 import tracking from '~/util/tracking';
 import { useBreadcrumbStore } from '~/stores/breadcrumb';
 import Footer from '~/components/Footer.vue';
