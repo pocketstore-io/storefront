f00d4abc6c2bd2cf6a54f084b4ab05b1b0c077f2
jm@habit-streak.app <support@habit-streak.app>
2025-04-15 21:49:58 +0200

    fix hero content

--- components/login/Hero.vue
+++ components/login/Hero.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="overflow-hidden bg-white py-6 rounded-lg grid grid-cols-6">
-    <LoginCardHero2 />
+    <LoginContent />
     <div class="col-span-6 md:col-span-3 px-8">
       <LoginCardRegisterForm v-if="form == 'register'" />
       <LoginCardLoginForm v-else />
     </div>
   </div>
 </template>
+
+<script setup lang="ts">
+const form = ref('register');
+</script>
