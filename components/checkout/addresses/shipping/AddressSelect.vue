476638c69adfcb57c308e178482290214bf9225a
jm@habit-streak.app <support@habit-streak.app>
2025-04-18 20:47:35 +0200

    split shipping into usecase

--- components/checkout/addresses/shipping/AddressSelect.vue
+++ components/checkout/addresses/shipping/AddressSelect.vue
@@ -7,6 +7,7 @@
 
 <script lang="ts" setup>
 import { useLocalStorage } from '@vueuse/core';
+import { select, validation } from '~/usecase/checkout/shipping';
 import { usePocketBase } from '~/util/pocketbase';
 
 const addressess = ref([]);
@@ -29,58 +30,10 @@ onMounted(async () => {
 });
 
 watch(selectedShippingAddress, (value) => {
-  if (value == 'new-one') {
-    shipping.value.name = '';
-    shipping.value.surname = '';
-    shipping.value.street = '';
-    shipping.value.number = 1;
-    shipping.value.city = '';
-    shipping.value.zip = '';
-    shipping.value.country = 'de';
-  }
-  else {
-    addressess.value.map((item) => {
-      if (item.id = value) {
-        shipping.value.name = item.name.split(' ')[0];
-        shipping.value.surname = item.name.split(' ')[1];
-        shipping.value.street = item.street;
-        shipping.value.number = item.number;
-        shipping.value.city = item.city;
-        shipping.value.zip = item.postcode;
-        shipping.value.country = item.country;
-      }
-    })
-  }
+  select(value, shipping);
 });
 
 watch(shipping, () => {
-  if (shipping.value.city == '') {
-    valid.value.shipping = false;
-    return
-  }
-  if (shipping.value.name == '') {
-    valid.value.shipping = false;
-    return
-  }
-  if (shipping.value.number < 1) {
-    valid.value.shipping = false;
-    return
-  }
-  if (shipping.value.street == '') {
-    valid.value.shipping = false;
-    return
-  }
-  if (shipping.value.surname == '') {
-    valid.value.shipping = false;
-    return
-  }
-  if (shipping.value.zip == '') {
-    valid.value.shipping = false;
-    return
-  }
-  valid.value.shipping = true;
+  validation(shipping, valid)
 }, { deep: true });
-
 </script>
-
-<style></style>
---
+++ usecase/checkout/shipping.ts
@@ -0,0 +1,52 @@
+export const select = (value,shipping) =>{
+    if (value == 'new-one') {
+        shipping.value.name = '';
+        shipping.value.surname = '';
+        shipping.value.street = '';
+        shipping.value.number = 1;
+        shipping.value.city = '';
+        shipping.value.zip = '';
+        shipping.value.country = 'de';
+      }
+      else {
+        addressess.value.map((item) => {
+          if (item.id = value) {
+            shipping.value.name = item.name.split(' ')[0];
+            shipping.value.surname = item.name.split(' ')[1];
+            shipping.value.street = item.street;
+            shipping.value.number = item.number;
+            shipping.value.city = item.city;
+            shipping.value.zip = item.postcode;
+            shipping.value.country = item.country;
+          }
+        })
+      }
+}
+
+export const validation = (shipping,valid)=>{
+    if (shipping.value.city == '') {
+        valid.value.shipping = false;
+        return
+      }
+      if (shipping.value.name == '') {
+        valid.value.shipping = false;
+        return
+      }
+      if (shipping.value.number < 1) {
+        valid.value.shipping = false;
+        return
+      }
+      if (shipping.value.street == '') {
+        valid.value.shipping = false;
+        return
+      }
+      if (shipping.value.surname == '') {
+        valid.value.shipping = false;
+        return
+      }
+      if (shipping.value.zip == '') {
+        valid.value.shipping = false;
+        return
+      }
+      valid.value.shipping = true;
+}
