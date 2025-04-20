844c06d45f954e07530b4467e6a5369bfbb375bd
Jonathan Martz <support@habit-streak.app>
2025-04-20 20:36:17 +0200

    ttrigger custom event wuith plausbile

--- util/tracking.ts
+++ util/tracking.ts
@@ -1,5 +1,14 @@
 export default {
     async trackPage() {
-        console.log('track page');
+        if (typeof window !== 'undefined' && window.plausible) {
+            window.plausible('CustomEventName', {
+              props: {
+                key1: 'value1',
+                key2: 'value2'
+              }
+            });
+          } else {
+            console.warn('Plausible is not initialized');
+          }
     }
 }
