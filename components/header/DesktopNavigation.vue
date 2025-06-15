f318150e4bc03970b89991d75fdf65926a59e114
Jonathan Martz <github@jmartz.de>
2025-06-15 13:27:07 +0200

    update link cart

--- components/header/DesktopNavigation.vue
+++ components/header/DesktopNavigation.vue
@@ -3,25 +3,30 @@
     <ul class="flex space-x-6 text-sm font-bold items-center mr-6">
       <li>Startseite</li>
       <li>
-          <a href="https://www.pocketstore.io/" target="_blank">Docs</a>
+        <a href="https://www.pocketstore.io/" target="_blank">Docs</a>
       </li>
       <li>
-        <a href="https://www.pocketstore.io/shops/">
-          Shops
-        </a>
+        <a href="https://www.pocketstore.io/shops/"> Shops </a>
       </li>
     </ul>
     <ul class="flex space-x-3">
       <li>
-        <a href="https://github.com/pocketstore-io/demo" target="_blank" class="flex justify-center items-center btn btn-primary text-white">
+        <a
+          href="https://github.com/pocketstore-io/demo"
+          target="_blank"
+          class="flex justify-center items-center btn btn-primary text-white"
+        >
           <FontAwesomeIcon size="2x" class="w-5" :icon="faGithub" />
         </a>
       </li>
-            <li>
-        <button class="flex justify-center items-center btn btn-neutral">
+      <li>
+        <a
+          href="/de/checkout"
+          class="flex justify-center items-center btn btn-neutral"
+        >
           <FontAwesomeIcon size="1x" :icon="faShoppingCart" />
           <span class="ml-2">0</span>
-        </button>
+        </a>
       </li>
       <li>
         <button class="flex justify-center items-center btn btn-neutral">
@@ -35,6 +40,6 @@
 
 <script setup lang="ts">
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-import { faShoppingCart,faHeart } from "@fortawesome/free-solid-svg-icons";
+import { faShoppingCart, faHeart } from "@fortawesome/free-solid-svg-icons";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 </script>
