b7f913b68760d2204bb76d0f2d4d27ee481296f3
Jonathan Martz <github@jmartz.de>
2025-06-21 17:42:29 +0200

    add product caching

--- components/catalog/ProductCard.vue
+++ components/catalog/ProductCard.vue
@@ -2,16 +2,30 @@
   <div v-if="product" class="card shadow-xl bg-white">
     <figure>
       <a :href="'/' + locale + '/product/' + product.slug + '.html'">
-        <img :src="'https://' + config.domain + '/api/files/' + product.collectionId + '/' + product.id + '/' + product.cover"
-          alt="Shoes">
+        <img
+          :src="
+            'https://' +
+            config.domain +
+            '/api/files/' +
+            product.collectionId +
+            '/' +
+            product.id +
+            '/' +
+            product.cover
+          "
+          alt="Shoes"
+        />
       </a>
     </figure>
     <div class="card-body">
       <h2 class="card-title">{{ product.name }}</h2>
       <p class="text-ellipsis line-clamp-2">{{ product.description }}</p>
       <div class="card-actions join gap-0 justify-end">
-        <a v-if="product.price" :href="'/' + locale + '/product/' + product.slug + '.html'"
-          class="btn btn-primary join-item">
+        <a
+          v-if="product.price"
+          :href="'/' + locale + '/product/' + product.slug + '.html'"
+          class="btn btn-primary join-item"
+        >
           {{ product.price.toFixed(2) }} €
         </a>
         <CatalogAddToCart :product="product" />
@@ -22,22 +36,45 @@
 
 <script lang="ts" setup>
 import { usePocketBase } from "~/util/pocketbase";
-import config from '@/pocketstore.json'
-import ProductGrid from "./ProductGrid.vue";
+import config from "@/pocketstore.json";
+import { useLocalStorage } from "@vueuse/core";
+
 const pb = usePocketBase();
 
 const i18n = useI18n();
 const locale = i18n.locale;
 
-const { identifier } = defineProps({
+const props = defineProps({
   identifier: { type: String, requiered: true },
 });
 
-const product = ref({});
+const product = useLocalStorage("product-" + props.identifier + "", {}, {});
+const date = useLocalStorage(
+  "product-" + props.identifier + "-date",
+  new Date().toLocaleDateString("de"),
+  {}
+);
+
+function isEmpty(obj) {
+  for (const prop in obj) {
+    if (Object.hasOwn(obj, prop)) {
+      return false;
+    }
+  }
+
+  return true;
+}
 
 onMounted(async () => {
-  product.value = await pb
-    .collection("products")
-    .getFirstListItem('slug="' + identifier + '"');
+  if (
+    product.value === null ||
+    date.value != new Date().toLocaleDateString("de") ||
+    isEmpty(product.value)
+  ) {
+    console.log('fetch abc')
+    product.value = await pb
+      .collection("products")
+      .getFirstListItem('slug="' + props.identifier + '"');
+  }
 });
 </script>
