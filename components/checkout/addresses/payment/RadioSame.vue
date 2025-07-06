c77818705660c236d85a9893778ce37ba87fe64b
Jonathan Martz <github@jmartz.de>
2025-07-06 10:34:06 +0200

    fix radio same with v-model

--- components/checkout/addresses/payment/RadioSame.vue
+++ components/checkout/addresses/payment/RadioSame.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="alert border-black alert-outline mb-3">
     <label class="label" for="same">
-      <input type="checkbox" checked="checked" class="toggle" />
+      <input type="checkbox" v-model="same" checked="checked" class="toggle" />
       <span>{{ $t('checkout.same') }}</span>
     </label>
   </div>
