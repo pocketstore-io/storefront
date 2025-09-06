853f4b2acecce1fff8dd833292da09da7a460bf2
jonathan-martz <github@jmartz.de>
2025-09-06 21:04:48 +0200

    reformat code

--- components/catalog/ProductCard.vue
+++ components/catalog/ProductCard.vue
@@ -2,42 +2,60 @@
   <div v-if="product" class="card shadow-xl bg-white">
     <figure>
       <a :href="'/' + locale + '/product/' + product.slug + '.html'">
-        <img :src="'https://' + config.domain + '/api/files/' + product.collectionId + '/' + product.id + '/' + product.cover"
-          alt="Shoes">
+        <img
+            :src="'https://' + config.domain + '/api/files/' + product.collectionId + '/' + product.id + '/' + product.cover"
+            alt="Shoes">
       </a>
     </figure>
     <div class="card-body">
       <h2 class="card-title">{{ product.name }}</h2>
       <p class="text-ellipsis line-clamp-2">{{ product.description }}</p>
-      <div class="card-actions join gap-0 justify-end">
-        <a v-if="product.price" :href="'/' + locale + '/product/' + product.slug + '.html'"
-          class="btn btn-primary join-item">
-          {{ product.price.toFixed(2) }} €
-        </a>
-        <CatalogAddToCart :product="product" />
+      <section>
+        <button v-if="stock === null || stock.quantity === 0" class="btn btn-error">
+          {{$t('catalog.product.stock-empty')}}
+        </button>
+        <button v-if="stock !== null && stock.quantity > 0" class="btn btn-success">
+          {{$t('catalog.product.stock')}}: {{ stock.quantity }}
+        </button>
+      </section>
+      <div class="card-actions join gap-0 flex justify-end">
+        <section>
+          <a v-if="product.price" :href="'/' + locale + '/product/' + product.slug + '.html'"
+             class="btn btn-primary join-item">
+            {{ product.price.toFixed(2) }} €
+          </a>
+          <CatalogAddToCart :product="product"/>
+        </section>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { usePocketBase } from "~/util/pocketbase";
+import {usePocketBase} from "~/util/pocketbase";
 import config from '@/pocketstore.json'
-import ProductGrid from "./ProductGrid.vue";
+
 const pb = usePocketBase();
 
 const i18n = useI18n();
 const locale = i18n.locale;
 
-const { identifier } = defineProps({
-  identifier: { type: String, requiered: true },
+const {identifier} = defineProps({
+  identifier: {type: String, requiered: true},
 });
 
 const product = ref({});
+const stock: Ref = ref({});
 
 onMounted(async () => {
+  pb.autoCancellation(false)
   product.value = await pb
-    .collection("products")
-    .getFirstListItem('slug="' + identifier + '"');
+      .collection("products")
+      .getFirstListItem('slug="' + identifier + '"');
+  stock.value = (await pb
+      .collection("product_stocks")
+      .getList(1, 1, {
+        "filter": 'product="' + identifier + '"'
+      })).items[0] ?? null;
 });
 </script>
