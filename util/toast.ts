8b30d0d4df39378e3ed251b6dc569d5477df20aa
jonathan-martz <github@jmartz.de>
2025-09-07 17:05:10 +0200

    update component and util for messages

--- util/toast.ts
+++ util/toast.ts
@@ -1,16 +1,22 @@
-import { useLocalStorage } from "@vueuse/core"
+import { ref } from "vue";
+import { useLocalStorage } from "@vueuse/core";
 import { v7 as uuidv7 } from 'uuid';
 
-export const toasts = useLocalStorage('toasts', [], {});
+export type ToastMessage = {
+    message: string;
+    id: string;
+    timer: number;
+    status: string;
+};
 
-export const add = (message,status) => {
-  let id = uuidv7();
-  toasts.value.push({
-    id,
-    message,
-    status
-  });
-  setTimeout(() => {
-    toasts.value = toasts.value.filter((item) => item.id != id)
-  }, 5000)
-}
+// Correctly type the ref as an array of ToastMessage
+export const messages: Ref<ToastMessage[]> = useLocalStorage('messages', [], {});
+
+export const addToast = (message: string, status: string) => {
+    const id = uuidv7();
+    const unixNowPlus5 = Math.floor(Date.now() / 1000) + 5;
+    messages.value.push({ id, message, status,timer: unixNowPlus5 }); // Don't reassign; just push
+    setTimeout(() => {
+        messages.value = messages.value.filter(item => item.id !== id);
+    }, 5000);
+};
