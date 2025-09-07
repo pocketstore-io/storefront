9dea5be731e1dcace91b27e20f1d13c4e89d0898
jonathan-martz <github@jmartz.de>
2025-09-07 17:42:52 +0200

    add toast as default message

--- components/catalog/AddToCart.vue
+++ components/catalog/AddToCart.vue
@@ -7,13 +7,14 @@
 <script lang="ts" setup>
 import { useLocalStorage } from "@vueuse/core";
 import { usePocketBase } from "~/util/pocketbase";
+import {addToast} from "../../util/toast";
 // TODO release date button, stock
 
 const { product } = defineProps({
-    product: {
-        type: Object,
-        requiered: true,
-    },
+  product: {
+    type: Object,
+    requiered: true,
+  },
 });
 
 const pb = usePocketBase();
@@ -21,12 +22,12 @@ const pb = usePocketBase();
 const cart = useLocalStorage("cart", [], {});
 
 const addToCart = async (id, qty = 1) => {
-    if (!cart.value) {
-        cart.value = [];
-    }
+  if (!cart.value) {
+    cart.value = [];
+  }
 
-    // Check if the product already exists in the cart
-    const existingItem = cart.value.find((item) => item.id === id);
-    // TODO messages
+  // Check if the product already exists in the cart
+  const existingItem = cart.value.find((item) => item.id === id);
+  addToast('Produkt hinzugefügt.', 'success')
 };
 </script>
