e29890b4f7cda27b75391d5c267e3445df1b6129
jonathan-martz <github@jmartz.de>
2025-11-13 21:48:01 +0100

    update impressum

--- components/impressum/5TMG.vue
+++ components/impressum/5TMG.vue
@@ -1,5 +1,5 @@
 <template>
   <h2><strong>Angaben gemäß § 5 TMG</strong></h2>
-  <p>Jonathan Martz Software Entwicklung<br> Othestrasse 2<br> 51702 Bergneustadt <br><br></p>
+  <p>Jonathan Martz Software Entwicklung<br> Talstrasse 26a<br> 51702 Bergneustadt <br><br></p>
 
 </template>
