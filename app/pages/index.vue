ae818d5f220c73f366cad206d9d24177a64f4d02
jonathan-martz <github@jmartz.de>
2025-11-19 22:41:56 +0100

    fix second breadcrumbs import

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,7 @@
 </template>
 
 <script setup lang="ts">
-import { addBreadcrumb } from "~/utils/breadcrumbs";
+import { addBreadcrumb } from "~/utils/breadcrumb";
 const { t } = useI18n();
 useHead({
     title: t("general.title"),
