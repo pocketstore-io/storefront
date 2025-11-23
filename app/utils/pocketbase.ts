f70b64e6cb52936a31152ef457a47f7ee2b73458
jonathan-martz <github@jmartz.de>
2025-11-23 21:22:45 +0100

    add getMediaUrl for pb

--- app/utils/pocketbase.ts
+++ app/utils/pocketbase.ts
@@ -1,8 +1,12 @@
-import PocketBase from "pocketbase";
+import PocketBase, {type RecordModel} from "pocketbase";
 import config from "../pocketstore.json";
 
 const url = "https://" + config.domains.pocketbase;
 const pb = new PocketBase(url);
+
+export const getMediaUrl = (media: RecordModel, field: String) => {
+    return url + '/api/files/' + media.collectionId + '/' + media.id + '/' + media[field];
+}
 
 export const usePocketBaseUrl = () => {
     return url;
