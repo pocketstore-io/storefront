004bca9e842b98bfc4378e39fe5a0033849bed81
jonathan-martz <github@jmartz.de>
2026-03-10 20:40:44 +0100

    remove unpkash image and maintance mode

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <section v-if="!config.maintance.enabled" class="page min-h-screen" data-theme="light">
+  <section class="page min-h-screen" data-theme="light">
     <header>
       <Header/>
     </header>
@@ -15,30 +15,6 @@
     <footer>
       <Footer/>
     </footer>
-  </section>
-  <section v-else class="bg-gray-400 min-h-screen w-full px-3 py-3 flex items-center justify-center">
-    <div class="card shadow-sm max-w-md bg-gray-300">
-      <figure>
-        <img
-            src="/unsplash/christopher-gower-m_HRfLhgABo-unsplash.jpg"
-            alt="Shoes"/>
-      </figure>
-      <div class="card-body">
-        <h2 class="card-title">Dieser Shop ist aktuell im Wartungsmodus</h2>
-        <p>
-          Wir warten gerade unserm Shop System von
-          <a href="https://PocketStore.io" class="text-blue-400">PocketStore.io</a>
-          für noch bessere Einkaufsmöglichkeiten und Support
-          unserer Kunden.
-        </p>
-        <div class="card-actions justify-end mt-3">
-          <p class="block text-center alert alert-info text-white">
-            Wie gewohnt ab erreichbar ab {{ config.maintance.until }}
-          </p>
-          <button class="btn btn-block btn-secondary">Benachrichtigung per Mail</button>
-        </div>
-      </div>
-    </div>
   </section>
 </template>
 <script setup lang="ts">
